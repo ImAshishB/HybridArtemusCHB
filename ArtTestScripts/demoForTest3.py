85beb937a0da8367b7b22670035d5d7d07666f5a
ImAshishB <114209574+ImAshishB@users.noreply.github.com>
2024-04-01 16:12:22 +0530

    Added new Test Files 2

--- ArtTestScripts/demoForTest3.py
+++ ArtTestScripts/demoForTest3.py
@@ -1,6 +1,9 @@
+# In this Script, script will be record errors and will be continuing for next script.
+import logging
+import datetime
 from selenium import webdriver
 from selenium.common import NoSuchElementException, ElementNotVisibleException, ElementNotSelectableException, \
-    ElementClickInterceptedException, ElementNotInteractableException, NoAlertPresentException, TimeoutException
+    ElementClickInterceptedException, ElementNotInteractableException, NoAlertPresentException
 from selenium.webdriver import ActionChains, Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -9,12 +12,28 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from utilites.utils import utills
 import time
+import string
+import random
 
+# # Configure logger
+# logging.basicConfig(filename='Artemus.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+#
+# # Create a timestamp for the log file name
+# timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+#
+# # Create the log file name with the timestamp
+# log_file = f"Artemus_{timestamp}.log"
+#
+# # Configure logger
+# logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+logging.basicConfig(filename="Art_7501_1Entry", level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', filemode='w')
+logging.info('------------------------------------------------------------New Log Started From Here-----------------------------------------------------------------------')
 serv_obj = Service("C:\Drivers\chromedriver-win64\chromedriver.exe")
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=options, service=serv_obj)
-mywait = WebDriverWait(driver, 10, poll_frequency=2, ignored_exceptions=[NoSuchElementException,
+mywait = WebDriverWait(driver, 10, poll_frequency=2, ignored_exceptions= [NoSuchElementException,
                                                                          ElementNotVisibleException,
                                                                          ElementNotSelectableException,
                                                                          ElementClickInterceptedException,
@@ -23,62 +42,105 @@ mywait = WebDriverWait(driver, 10, poll_frequency=2, ignored_exceptions=[NoSuchE
 
 # URL
 driver.get("http://52.54.244.138:8080/ArtemusChb/")
-# driver.get("https://chb.artemusgroupusa.com/")
+
+
 # Maximize page
 driver.maximize_window()
+# file = "D:\Artmus Spec\Automation_Artemus\TestML.xlsx"
+# path = ".//testdata/TestML.xlsx"
+path = "D:\Artmus Spec\Automation_Artemus\TestML.xlsx"
 
-# Login
-driver.find_element(By.ID, "username").send_keys("Ashishh")
-driver.find_element(By.ID, "password").send_keys("ashishh1")
-# driver.find_element(By.ID, "username").send_keys("Varada")
-# driver.find_element(By.ID, "password").send_keys("varada1")
-driver.find_element(By.XPATH, '//*[@id="background"]/div/div/div/div/div/form/button').click()
-file = "D:\Artmus Spec\Automation_Artemus\querylist.xlsx"
-# HomePage
-SHORT_TIMEOUT = 1  # give time for the loading element to appear
-LONG_TIMEOUT = 30  # give time for loading to finish
-LOADING_ELEMENT_XPATH = "//body//app-root//app-loader//h3[@class='loadingScreen__text']"
-try:
-    WebDriverWait(driver, LONG_TIMEOUT).until(EC.invisibility_of_element_located((By.XPATH, LOADING_ELEMENT_XPATH)))
-except TimeoutException:
-    pass
-Shipment = mywait.until(EC.element_to_be_clickable((By.LINK_TEXT, "Shipments")))
-Shipment.click()
-time.sleep(2)
+def random_invoceGenerator(size=5, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for x in range(size))
+def random_BillGenerator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for x in range(size))
+#Read data from excel
+for r in range(3, 4):
+    i = 0
+    i = i - 1
+    selectImporterData = utills.readData(path, "Sheet1", r, 140)
+    # intlineitmscount = int(lineitmscount)
 
-try:
-    WebDriverWait(driver, LONG_TIMEOUT).until(EC.invisibility_of_element_located((By.XPATH, LOADING_ELEMENT_XPATH)))
-except TimeoutException:
-    pass
-# Select Importer
-selectImporterTxt = mywait.until(EC.element_to_be_clickable((By.XPATH, "//input[@id='typeahead-basic']")))
-selectImporterTxt.click()
-selectImporterTxt.send_keys("arttest")
-time.sleep(2)
-selectImporterTxt.send_keys(Keys.ENTER)
-time.sleep(2)
-nextarroro=driver.find_element(By.XPATH, "//i[@class='fa fa-arrow-circle-o-right fa-2x']")
-rows=driver.find_element(By.XPATH, "//table/tbody/tr").text # count number of rows
-cols=driver.find_element(By.XPATH, "//table/thead/tr/th").text # count number of coloumns
-rowsCount=len(driver.find_elements(By.XPATH, "//table/tbody/tr")) # count number of rows
-colsCount=len(driver.find_elements(By.XPATH, "//table/thead/tr/th")) # count number of coloumns
-print("Total number of rows are:",rows)
-print("Total number of coloumns are:",cols)
-print("Total number of rows are:",rowsCount)
-print("Total number of coloumns are:",colsCount)
+    # #Login
+    usern = utills.readData(path, "Sheet1", r, 5)
+    pasw = utills.readData(path, "Sheet1", r, 6)
 
-for r in range(1, rowsCount + 1):
-    for c in range(1, colsCount + 1):
-        values = driver.find_element(By.XPATH, "//table/tbody/tr[" + str(r) + "]/td[" + str(c) + "]").text
-        # print(values)
-        # utills.writeData(file, "Sheet3", r, c, values)
-driver.close()
-# a = 4
-# for _ in range(a):
-#
-#     nextarroro.click()
-#     try:
-#         WebDriverWait(driver, LONG_TIMEOUT).until(EC.invisibility_of_element_located((By.XPATH, LOADING_ELEMENT_XPATH)))
-#     except TimeoutException:
-#         pass
+    vesselsname = utills.readData(path, "Sheet1", r, 15)
+    vessellsno = utills.readData(path, "Sheet1", r, 16)
+    containerscount = utills.readData(path, "Sheet1", r, 4)
+    containerlist = utills.readData(path, "Sheet1", r, 17).split(",")
+
+    # Passing the data in application
+    # ----------------------------------
+
+    # Login
+    login_username = mywait.until(EC.element_to_be_clickable((By.ID, "username")))
+    login_username.click()
+    login_username.send_keys(usern)
+    login_password = mywait.until(EC.element_to_be_clickable((By.ID, "password")))
+    login_password.click()
+    login_password.send_keys(pasw)
+    driver.find_element(By.XPATH, '//*[@id="background"]/div/div/div/div/div/form/button').click()
+    print("Login Done")
+    logging.info("Login Done")
+
+    # HomePage
+    time.sleep(4)
+    Shipment = mywait.until(EC.element_to_be_clickable((By.LINK_TEXT, "Shipments")))
+    Shipment.click()
+    time.sleep(2)
+
+    # Select Importer
+    selectImporterTxt = mywait.until(EC.element_to_be_clickable((By.XPATH, "//input[@id='typeahead-basic']")))
+    selectImporterTxt.click()
+    selectImporterTxt.send_keys(selectImporterData)
+    time.sleep(2)
+    selectImporterTxt.send_keys(Keys.ENTER)
+
+
+    Form7501 = mywait.until(EC.element_to_be_clickable((By.LINK_TEXT, "Form 7501")))
+    Form7501.click()
+    time.sleep(1)
+    print("7501 Form opened")
+    logging.info("7501 Form opened")
+    try:
+        try:
+            vesselNametxt = driver.find_element(By.CSS_SELECTOR, "input#vesselName")
+            vesselNametxt.send_keys(vesselsname)
+        except:
+            pass
+
+        vesselFlightNotxt = driver.find_element(By.CSS_SELECTOR, "input#vesselFlightNo")
+        vesselFlightNotxt.send_keys(vessellsno)
+
+        try:
+            Continer = driver.find_elements(By.XPATH, "// a[contains(text(), 'Add/Edit container')]")
+            if Continer:
+                driver.find_element(By.XPATH, "// a[contains(text(), 'Add/Edit container')]").click()
+                for cont in zip(containerlist):
+                    driver.find_element(By.XPATH,
+                                        "//app-vessel-container//div[@class='row new-form-row'][1]//div[@class='col-md-4 form-lable'][1]//input[@type='text']").send_keys(
+                        cont)
+                    time.sleep(1)
+                    driver.find_element(By.XPATH,
+                                        "//app-vessel-container//div[@class='row new-form-row'][4]//button[normalize-space()='Add new container']").click()
+                    time.sleep(1)
+                driver.find_element(By.XPATH, "//button[@type='button'][normalize-space()='Save']").click()
+        except:
+            print("No Container found")
+            pass
+
+
+
+
+        # Check if all data filled in Vessel information or not
+        if vesselNametxt.get_attribute("value") and vesselFlightNotxt.get_attribute("value"):
+            print("----Vessel Done----")
+            logging.info("----Vessel Done----")
+        else:
+            print("!----Vessel Not Done----!")
+            logging.error("!----Vessel Not Done----!")
+
+    except Exception as e:
+        pass
 
--- testcases/test_EntryTC5.py
+++ testcases/test_EntryTC5.py
@@ -15,8 +15,8 @@ from base.base_driver import BaseDriver
 from utilites.utils import utills
 
 @pytest.mark.usefixtures("setup")
-class Test_EntrySummary1():
-    randomInvoice = "ABTstTC1_Vsl_"+ utills.random_invoceGenerator() # random_invoceGenerator() came from utils
+class Test_EntrySummary5():
+    randomInvoice = "ABTstTC5_Rail_"+ utills.random_invoceGenerator() # random_invoceGenerator() came from utils
     randomBill = "M" + utills.random_BillGenerator()  # random_BillGenerator() came from utils
     file = "D:/Artmus Spec/Automation_Artemus/TestML.xlsx"
     log = utills.custom_logger()
@@ -30,10 +30,10 @@ class Test_EntrySummary1():
         self.getvalues = getAtributesOfText(self.driver, self.mywait)
 
 
-    def test_TC1_SimpleEntry_Vessel_Container(self): # Country of origin india/No Duty HTS(NDC)
-        self.log.info("----------------Test Case test_TC1_SimpleEntry_Vessel_Container with Container Starterd----------------")
+    def test_TC5_SimpleEntry_Rail_Container(self): # Country of origin india/No Duty HTS(NDC)
+        self.log.info("----------------Test Case test_TC5_SimpleEntry_Rail_Container with Container Starterd----------------")
 
-        for r in range(3, 4):
+        for r in range(7, 8):
             self.selectIMPORTERData = utills.readData(self.file, 'TcHybridArtemusData', r, 140)
             self.addBillbutton = utills.readData(self.file, 'TcHybridArtemusData', r, 2)
             self.lineitmscount = utills.readData(self.file, 'TcHybridArtemusData', r, 3)
@@ -193,19 +193,17 @@ class Test_EntrySummary1():
                 else:
                     self.log.error("----The values are not calculated properly----")
             else:
-                # self.driver.save_screenshot(".\\screenshots\\" + "test_HTC1_scr.png")  # Screenshot
+                # self.driver.save_screenshot(".\\screenshots\\" + "test_HTC5_scr.png")  # Screenshot
                 self.esf.formSavedConfirmationMsg()
                 self.log.error("----Form Not Saved. Test Failed----")
 
-        self.log.info("----------------Test Case test_TC1_SimpleEntry_Vessel_Container with Container End----------------")
+        self.log.info("----------------Test Case test_TC5_SimpleEntry_Rail_Container with Container End----------------")
 
 
 
-# pytest -v -s testcases/test_EntryTC1.py
-# pytest -v -s testcases/test_EntryTC1.py --browser chrome
-# pytest -v -s testcases/test_EntryTC1.py --browser firefox
-# pytest -v -s --html=reports\EntryTC1Report.html testcases/test_EntryTC1.py
-# pytest -v --html=reports\EntryTC1Report.html testcases/test_EntryTC1.py
-# pytest -v --html=reports\EntryTC1Report.html testcases/test_EntryTC1.py --browser chrome   #if in html report if logs are not getting genrated then remove -s and try
-# pytest -v -s -n=1 --html=reports\report.html testcases/test_Login.py    n=1 means each test cases running 1 by 1
-# pytest -v -s -n=2 --html=reports\report.html testcases/test_Login.py    n=2 means 2 test cases running at a time in 2 browsers
+# pytest -v -s testcases/test_EntryTC5.py
+# pytest -v -s testcases/test_EntryTC5.py --browser chrome
+# pytest -v -s testcases/test_EntryTC5.py --browser firefox
+# pytest -v -s --html=reports\EntryTC5Report.html testcases/test_EntryTC5.py
+# pytest -v --html=reports\EntryTC5Report.html testcases/test_EntryTC5.py
+# pytest -v --html=reports\EntryTC5Report.html testcases/test_EntryTC5.py --browser chrome   #if in html report if logs are not getting genrated then remove -s and try
